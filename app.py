326bae2d75ce408baab3feab06101255ad72597b
vgarita13 <vrita7x@gmail.com>
2026-05-24 10:13:31 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -2,6 +2,11 @@ import streamlit as st
 import pandas as pd
 from supabase import create_client
 
+SUPABASE_URL = st.secrets["SUPABASE_URL"]
+SUPABASE_KEY = st.secrets["SUPABASE_KEY"]
+
+supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
+
 # ------------------------
 # OLDAL BEÁLLÍTÁSOK
 # ------------------------
