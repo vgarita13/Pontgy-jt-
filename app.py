97f7760718813617b9f8eeeb79377a9556ac6e3d
vgarita13 <vrita7x@gmail.com>
2026-05-26 15:52:11 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -757,7 +757,7 @@ if st.sidebar.button("⬅️ Csoportváltás"):
 st.sidebar.markdown(f"""
 <div style="
     margin-top:-8px;
-    margin-bottom:35px;
+    margin-bottom:10px;
     background:rgba(255,255,255,0.12);
     border:1px solid rgba(255,255,255,0.18);
     border-radius:18px;
